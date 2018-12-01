c70c42671e5c46e09123909f0267d8c7041d7006
dexter-morgan <32188374+dexter-morgan@users.noreply.github.com>
2018-12-01 19:49:40 +0530

    Create macchanger.py
    Python script to change the mac address of any interface on your device to any mac address.
    Usage example: python macchanger.py -i eth0 -m 00:00:00:00:00:00

---
+++ macchanger.py
@@ -0,0 +1,37 @@
+#!usr/bin/env python
+
+import subprocess
+import optparse
+import re
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i","--interface",dest = "interface", help = "Interface of which we change mac address")
+    parser.add_option("-m","--mac",dest = "new_mac", help = "New Mac")
+    (options, arguments) = parser.parse_args()
+    return options
+
+def change_mac(interface,new_mac):
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if mac_result:
+        return mac_result.group(0)
+    else:
+        print("No mac found")
+
+
+options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("current mac is "+ str(current_mac))
+
+change_mac(options.interface,options.new_mac)
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("Mac changed to " + current_mac)
+else:
+    print("Mac unchanged")
