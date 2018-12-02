9e628ef0397b1a269989495acd48baf322a2b34b
dexter-morgan <32188374+dexter-morgan@users.noreply.github.com>
2018-12-03 01:49:31 +0530

    Update network_scanner.py
    Added command line arguments

--- network_scanner.py
+++ network_scanner.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-#Example usage: python network_scanner.py
+#Example usage: python network_scanner.py --ip 10.0.2.1/24
 
 
 import scapy.all as scapy
+import argparse
 
 
 def scan(ip):
@@ -11,8 +12,23 @@ def scan(ip):
     broadcast   = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
     combined    = broadcast/arp_request
     answered_list = scapy.srp(combined, timeout = 1, verbose = False)[0]
-    print("IP\t\t\tMAC\n-------------------------------------------------------------")
+    clients_list = []
     for i in answered_list:
-        print(i[1].psrc + "\t\t" + i[1].hwsrc)
+        client_dict = {"ip": i[1].psrc, "mac": i[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 
-scan("10.0.2.8/24")
+def print_result(result_list):
+    print("IP\t\t\tMAC\n-------------------------------------------------------------")
+    for i in result_list:
+        print(i["ip"] + "\t\t" + i["mac"])
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i","--ip",dest = "ip", help = "Ip range for which Mac is to be found")
+    options = parser.parse_args()
+    return options
+
+options = get_arguments()
+clients_list = scan(options.ip)
+print_result(clients_list)
