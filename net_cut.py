a4cc2bcfff9a0b65b1a4434f08f52d3a8338a659
Darshil Desai <32188374+dexter-morgan@users.noreply.github.com>
2018-12-07 03:51:57 +0530

    Create net_cut.py

---
+++ net_cut.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python
+
+#Usage example: python net_cut.py # after running arp_spoof.py
+
+import netfilterqueue
+
+def process_packet(packet):
+    print(packet)
+    packet.drop()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
